3264ef47812e41d5eb93b13c7f8f7750e0cdb609
Eoghan Leddy <eleddy521@gmail.com>
2016-08-03 03:03:19 +0000

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,43 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+
+  def initialize(limit)
+
+    @num_arr = []
+    @sum_sq = 0
+    @sq_sum = 0
+    while(limit > 0)
+      @num_arr.push(limit)
+      limit-=1
+    end
+  end
+
+  def sum_of_squares
+
+    @num_arr.each do |x|
+      @sum_sq += x
+    end
+    @sum_sq = @sum_sq ** 2
+    @sum_sq
+
+  end
+
+  def square_of_sums
+
+    @num_arr.each do |x|
+      @sq_sum += (x ** 2)
+    end
+    @sq_sum
+
+  end
+
+  def difference
+
+    square_of_sums
+    sum_of_squares
+    answer = @sum_sq - @sq_sum
+    answer
+
+  end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,36 @@
 # Implement your procedural solution here!
+def sum_of_squares(array)
+
+  sum_all = 0
+  sum_sq = array.each do |x|
+    sum_all += (x ** 2)
+  end
+  sum_all
+
+end
+
+def square_of_sums(array)
+
+  sum_sqrd = 0
+  sqr_sum = array.each do |x|
+    sum_sqrd += x
+  end
+  sum_sqrd = sum_sqrd ** 2
+  sum_sqrd
+
+end
+
+def sum_square_difference(limit)
+
+
+  array_pass = []
+  while(limit > 0)
+    array_pass.push(limit)
+    limit -= 1
+  end
+  sq_of_sum = square_of_sums(array_pass)
+  sum_of_sq = sum_of_squares(array_pass)
+  answer = sq_of_sum - sum_of_sq
+  answer
+
+end
